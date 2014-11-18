9bd5cf302ffdbd9b15caf7dd3ae807ec1d34d029
binarier <binarier@me.com>
2014-11-19 00:34:14 +0800

    progress

---
+++ driver-hfr.h
@@ -0,0 +1,12 @@
+#ifndef HFR_H
+#define HFR_H
+
+#define MAX_CMD_LENGTH 100
+
+#define CMD_WRITE_REG 0x81
+#define CMD_READ_REG 0x42
+#define CMD_WRITE_WORK 0x24
+#define CMD_READ_NONCE 0x18
+
+
+#endif
